f587edbcc94afb0c4e5b00e6bff6c3a3bce645fd
venturions <alex.eduardo10@hotmail.com>
2023-10-08 16:42:02 -0300

    Categorizando os testes

---
+++ test/transactions.test.ts
@@ -0,0 +1,21 @@
+import { it, expect, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'New transaction', amount: 500, type: 'credit' })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
