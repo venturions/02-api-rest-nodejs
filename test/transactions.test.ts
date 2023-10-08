9010186299f2c93272de97ed783f7dac99c807fa
venturions <alex.eduardo10@hotmail.com>
2023-10-08 16:52:22 -0300

    Testando listagem de transações

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -18,4 +18,24 @@ describe('Transactions routes', () => {
 
     expect(response.statusCode).toEqual(201)
   })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'New transaction', amount: 500, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 500,
+      }),
+    ])
+  })
 })
